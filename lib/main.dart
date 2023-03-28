3d380a83163d475b404eaffbae6f1621c7cbe6e3
AnthraaaX <107903512+AnthraaaX@users.noreply.github.com>
2023-03-28 23:48:40 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Builder(
         builder: (context) => Scaffold(
           appBar: AppBar(
