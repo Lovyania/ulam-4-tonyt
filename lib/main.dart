e3b31cca8e9e383a7c3425b194050fdea210eaa4
AnthraaaX <107903512+AnthraaaX@users.noreply.github.com>
2023-04-07 18:19:31 +0800

    added side menu bar with minimal functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,101 @@ class _RecipeSearchBarState extends State<RecipeSearchBar> {
   }
 }
 
+class NavigationDrawer extends StatelessWidget {
+  const NavigationDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildHeader(context),
+              buildMenuItems(context),
+            ],
+          ),
+        ),
+      );
+
+  Widget buildHeader(BuildContext context) => Material(
+      color: Colors.blue.shade700,
+      child: InkWell(
+        onTap: () {
+          // User can click to navigate to user profile page
+          // Navigator.pop(context);
+          // Navigator.of(context).push(MaterialPageRoute(
+          //   builder: (context) => UserPage(),
+          // ));
+        },
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 24 + MediaQuery.of(context).padding.top,
+            bottom: 24,
+          ),
+          child: Column(
+            children: const [
+              CircleAvatar(
+                radius: 52,
+                backgroundImage: NetworkImage(
+                    'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/60320/angry-emoji-clipart-xl.png'),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'Flutter App',
+                style: TextStyle(fontSize: 28, color: Colors.white),
+              ),
+              Text(
+                'example@email.com',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ));
+  Widget buildMenuItems(BuildContext context) => Container(
+        padding: const EdgeInsets.all(24),
+        child: Wrap(
+          runSpacing: 16,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.home_outlined),
+              title: const Text('Home'),
+              onTap: () =>
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => MyApp(),
+              )),
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite_border),
+              title: const Text('Favorites'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.workspaces_outline),
+              title: const Text('Workflow'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.update),
+              title: const Text('Updates'),
+              onTap: () {},
+            ),
+            const Divider(color: Colors.black),
+            ListTile(
+              leading: const Icon(Icons.account_tree_outlined),
+              title: const Text('Plugins'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.notifications_outlined),
+              title: const Text('Notifications'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      );
+}
+
 class _MyAppState extends State<MyApp> {
   List<dynamic> recipeData = [];
 
@@ -54,6 +149,7 @@ class _MyAppState extends State<MyApp> {
           appBar: AppBar(
             title: Text('Recipe App'),
           ),
+          drawer: const NavigationDrawer(),
           body: Column(
             children: [
               RecipeSearchBar(),
