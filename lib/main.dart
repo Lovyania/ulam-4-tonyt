cf5e980a1f81baffa730e9d6bfa1f5a639c764bb
AnthraaaX <107903512+AnthraaaX@users.noreply.github.com>
2023-04-14 18:29:41 +0800

    added login and signup and connected to working database

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import 'package:ulam_4_tonyt/screens/login.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: FirebaseOptions(
+    apiKey: "AIzaSyBbMp8CIK3b752_Gv9_PQtLp9TM0n-5LbU",
+    appId: "1:202171125123:web:eda8295f04c38ae128bab6",
+    messagingSenderId: "202171125123",
+    projectId: "ulam4tonyt"
+  ));
+  runApp(MyApp());
+}
 
 
 class MyApp extends StatefulWidget {
